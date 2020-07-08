762b72083127946f901fb5c0ce4cf8df5a4c6353
AmerTm <amer.toumi@wethink.info>
2020-07-08 19:12:04 +0100

    first commit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './card.styles.css';
+
+const Card = (props) => (
+    <div className='card-container'>
+        <img alt ="monster" src={`https://robohash.org/${props.monster.id}IC.png?set=set2&size=180x180`}/>
+        <h2 >{props.monster.name}</h2>
+        <p>{props.monster.email}</p>
+    </div>
+)
+export default Card;
