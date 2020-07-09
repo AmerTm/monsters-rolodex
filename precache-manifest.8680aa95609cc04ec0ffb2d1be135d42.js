5dc2ae3f580ef3e638d6fb9d16cdc337a5bfd719
AmerTm <amer.toumi@wethink.info>
2020-07-09 10:31:27 +0100

    Updates

---
+++ precache-manifest.8680aa95609cc04ec0ffb2d1be135d42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80bee06ad7a594fdd5d5d20c3bafe5c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4aaae1d67cdc40a08d0e",
+    "url": "/monsters-rolodex/static/css/main.b4f1c6eb.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aaae1d67cdc40a08d0e",
+    "url": "/monsters-rolodex/static/js/main.e264bb00.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
